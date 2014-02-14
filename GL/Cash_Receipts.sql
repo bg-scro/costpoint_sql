7fb55cdb55907716cfaab52c638cfe2acd9d1222
Scrodaddy <bodom@three-wire.com>
2014-02-14 12:14:22 -0500

    Modify Cash Receipts script to add columns used in GSA-IFF calculation

--- GL/Cash_Receipts.sql
+++ GL/Cash_Receipts.sql
@@ -1,3 +1,6 @@
+-- NOTE:  This script is used to populate data for the GSA-IFF report
+-- Do not change structure without consulting the GSA-IFF calculator to
+-- confirm the data will still work properly.
 SELECT
 	hs.FY_CD
 	, hs.PD_NO
@@ -11,6 +14,15 @@ SELECT
 	, h.INVC_ID
 	, h.TRN_DESC
 	, h.TRN_AMT
+	, LEFT(h.PROJ_ID,5) AS 'TOP LEVEL PROJECT' 
+	, RIGHT(h.PROJ_ID,4) AS 'THIRD LEVEL PROJ'
+	, CASE
+		WHEN hs.PD_NO >= 1 AND hs.PD_NO <= 3 THEN 1
+		WHEN hs.PD_NO >= 4 AND hs.PD_NO <= 6 THEN 2
+		WHEN hs.PD_NO >= 7 AND hs.PD_NO <= 9 THEN 3
+		WHEN hs.PD_NO >= 10 AND hs.PD_NO <=12 THEN 4
+		ELSE 0
+	END AS 'QTR No'
 
 FROM DELTEKCP.DELTEK.CASH_RECPT_HDR_HS hs
 LEFT JOIN DELTEKCP.DELTEK.CASH_RECPT_TRN_HS h
